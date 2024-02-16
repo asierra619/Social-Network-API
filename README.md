9b3b13b3deb57f8d24ad37768195c61cd3cff472
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-02-15 17:36:56 -0800

    userController completed

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# social-network-api
+# Social Network API
+
+## Description
+
+
+## Table of Contents
+
+- [User Story](#user-story)
+- [Acceptance Criteria](#acceptance-criteria)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies Used](#technologies-used)
+- [License](#license)
+- [Credits](#credits)
+
+## User Story
+
+AS A social media startup<br />
+I WANT an API for my social network that uses a NoSQL database<br />
+SO THAT my website can handle large amounts of unstructured data
 
 ## Acceptance Criteria
 
@@ -8,6 +27,31 @@ THEN my server is started and the Mongoose models are synced to the MongoDB data
 WHEN I open API GET routes in Insomnia for users and thoughts<br />
 THEN the data for each of these routes is displayed in a formatted JSON<br />
 WHEN I test API POST, PUT, and DELETE routes in Insomnia<br />
-THEN I am able to successfully create, update, and delete users and thoughts in my database
-WHEN I test API POST and DELETE routes in Insomnia
-THEN I am able to successfully create and delete reactions to thoughts and add and remove friends to a user’s friend list
+THEN I am able to successfully create, update, and delete users and thoughts in my database<br />
+WHEN I test API POST and DELETE routes in Insomnia<br />
+THEN I am able to successfully create and delete reactions to thoughts and add and remove friends to a user’s friend list<br />
+
+## Installation
+
+
+
+GitHub Repo - 
+
+## Usage
+
+
+
+![screenshot of Code & Insomniac](/public/images/flower-moon.png)
+
+## Technologies Used
+
+Express, MongoDB/Mongoose, Nodemon
+
+## License
+
+MIT
+
+## Credits
+
+Thank You to the Following -<br />
+Bobbi Tarkany - Calendly Tutor
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const {User, Thought} = require('../models');
 
 module.exports = {
+  // Get All Users
   async getUsers(req, res) {
     try {
       const users = await User.find().populate('thoughts').populate('friends');
@@ -9,5 +10,62 @@ module.exports = {
       res.status(500).json(error);
     }
   },
-  
+
+  // Get Single User
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({_id: req.params.userId}).populate('thoughts').populate('friends');
+      if (!user) {
+        return res.status(404).json({ message: 'No User Found!' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  // Create User
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+  },
+
+    // Update User
+    async updateUser(req, res) {
+      try {
+        const user = await User.findOneAndUpdate(
+          {_id: req.params.userId},
+          {$set: req.body},
+          {runValidators: true, new: true}
+        );
+        if (!user) {
+          res.status(404).json({ message: 'No User Found!' });
+        }
+        res.json(User);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    },
+
+  // Delete User
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        res.status(404).json({ message: 'No User Found!' });
+      }
+
+      // Delete User's Thoughts When User Deleted
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User and Their Thoughts Thrown Into the Abyss!' });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
 }
