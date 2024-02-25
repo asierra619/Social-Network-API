295021d8af3dd509b004c6c4012bf4349f6c4afc
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-02-24 19:58:52 -0800

    edited route name, was missing an S

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  getThought,
+  getThoughts,
   getSingleThought,
   createThought,
   deleteThought,
@@ -9,7 +9,7 @@ const {
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
-router.route('/').get(getThought).post(createThought);
+router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
